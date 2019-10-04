ac1a83dff629f41863de730ec927b85ec38f7ca0
Enrique Cerda <jenrique.cerda@gmail.com>
2019-10-04 16:01:47 -0500

    Chapter 7

--- app/src/main/java/android/dynamic/com/CrimeActivity.java
+++ app/src/main/java/android/dynamic/com/CrimeActivity.java
@@ -1,6 +1,8 @@
 package android.dynamic.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
